0e5c153dd936b46f6a87e9b3e3eb27694da8fcb0
AminaIslam1912 <jenny191277@gmail.com>
2025-06-19 21:43:58 +0600

    added contact section

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -3,13 +3,14 @@
 import React from "react";
 import Image from "next/image";
 import Typewriter from 'typewriter-effect';
+import Link from "next/link";
 
 
 
 const Hero = () => {
   return (
 
-     <section className="text-black-600 bg-indigo-100">
+     <section className="text-black-600 bg-[#fae3c9]">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
@@ -28,10 +29,11 @@ const Hero = () => {
         I’m Amina Islam, a Computer Science and Engineering student at the University of Dhaka, with a passion for web and Android development. Proficient in Python, C++, Flutter, Java, JavaScript, React, Next.js, HTML, CSS, and Tailwind CSS, I’ve developed impactful projects like LearnLoop, a skill-sharing Android app built with Flutter, a Hotel Management System in Java, and Cosmo Shooter, a game developed using C-SDL. I thrive on crafting user-centric solutions that address real-world problems. When I'm not coding, I explore new tech trends and collaborate with peers. I’m eager to build innovative applications and contribute to the tech community.
       </p>
       <div className="flex justify-center">
+        <Link href="#contact">
         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
           Contact
         </button>
-        
+        </Link>
       </div>
     </div>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -5,7 +5,7 @@ import { FaDownload } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    
+    <div className="bg-[#ffd99f] z-50 sticky top-0">
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
           <Image
@@ -24,7 +24,7 @@ const Navbar = () => {
             <Link href="#" className="mr-5 hover:text-indigo-900 hover:underline font-bold">About</Link>
             <Link href="#" className="mr-5 hover:text-indigo-900 hover:underline font-bold">Skills</Link>
             <Link href="#" className="mr-5 hover:text-indigo-900 hover:underline font-bold" >Projects</Link>
-            <Link href="#" className="mr-5 hover:text-indigo-900 hover:underline font-bold">Contact</Link>
+            <Link href="#contact" className="mr-5 hover:text-indigo-900 hover:underline font-bold">Contact</Link>
           </nav>
           <a href="/assets/cv/My_CV.docx" target="_blank" rel="noopener noreferrer" className="flex items-center">
           <button className="inline-flex items-center bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-indigo-200 rounded text-base font-bold  text-white mt-4 md:mt-0 hover:text-black ">
@@ -36,6 +36,7 @@ const Navbar = () => {
 
         </div>
       </header>
+      </div>
       
   );
 };
